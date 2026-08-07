c4e0bd6faf5bf1286812f7db5ec83f2f15fc51e5
sk-mati <mati.sidoti.knap@gmail.com>
2026-08-07 17:34:39 -0300

    ejercicio7

---
+++ ejercicio7.py
@@ -0,0 +1,15 @@
+# Dada la edad de tres personas, edad1, edad2 y edad3, verifica si las tres edades son iguales. Muestra el resultado de la comparación.
+
+# Definición de las variables 
+
+edad1 = 100
+
+edad2 = 70
+
+edad3 = 30
+
+# Verificación de igualdad de valores
+
+print("\n") # Salto de línea
+print(edad1 == edad2 == edad3) # Imprime un booleano (True si son iguales, False si no lo son)
+print("\n")
