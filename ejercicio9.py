4a7d1d2eff401a8bb5f4572df81856ba654109dd
sk-mati <mati.sidoti.knap@gmail.com>
2026-08-07 17:50:12 -0300

    ejercicio9

---
+++ ejercicio9.py
@@ -0,0 +1,12 @@
+# Dadas dos palabras, palabra1 y palabra2, verifica si la longitud de la primera palabra es mayor que la de la segunda. Muestra el resultado de la comparación.
+
+# Definición de variables
+
+palabra1 = "La palabra de la variable uno es: palabra1,"
+
+palabra2 = "Y la palabra de la variable dos es: palabra2."
+
+# Comparación de longitudes
+
+print(len(palabra1) > len(palabra2)) 
+# Imprime True si la primera es mayor o False si es menor
