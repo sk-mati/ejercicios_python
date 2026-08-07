04b0047778f5a74c47eaedf50cd5af4317bc4b24
sk-mati <mati.sidoti.knap@gmail.com>
2026-08-07 17:09:40 -0300

    ejercicio5

---
+++ ejercicio5.py
@@ -0,0 +1,21 @@
+# Define cinco variables numéricas (int o float). Calcula el promedio de esos números y muestra el resultado en pantalla.
+
+# Define variables (con :sugerencia de tipos)
+
+numero_1:int = 3
+
+numero_2:int = 7
+
+numero_3:int = 9
+
+numero_4:float = 7.9
+
+numero_5:float = 1.3
+
+# Cálculo del promedio
+
+promedio = (numero_1 + numero_2 + numero_3 + numero_4 + numero_5) / 5
+
+# Muestra por pantalla
+
+print(f"\nEl promedio de los números es: {promedio}\n")
