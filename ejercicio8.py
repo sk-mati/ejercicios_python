03891900a5b6f53177cec4caea2197e536279b24
sk-mati <mati.sidoti.knap@gmail.com>
2026-08-07 17:41:54 -0300

    ejercicio8

---
+++ ejercicio8.py
@@ -0,0 +1,13 @@
+# Dados los precios de dos productos, precio_a y precio_b, verifica si el primer producto es más caro que el segundo. Muestra el resultado de la comparación.
+
+# Definición de variables
+
+precio_a = 100
+
+precio_b = 250
+
+# Verificación de precios
+
+print("\n", precio_a > precio_b, "\n")
+
+# Retorna True si es mayor, de lo contrario retornará False.
