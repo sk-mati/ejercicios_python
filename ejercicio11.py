fb4306f0b36d1f2f0d68fe58e9c9dbddee209ab1
sk-mati <mati.sidoti.knap@gmail.com>
2026-08-07 19:39:07 -0300

    ejercicio11

---
+++ ejercicio11.py
@@ -0,0 +1,15 @@
+# Dados los valores distancia_a y distancia_b, verifica si distancia_a es menor o igual que distancia_b. Muestra el resultado de la comparación.
+
+# Definición de variables 
+
+distancia_a = 13
+
+distancia_b = 27
+
+# Verificación de distancias por longitud
+
+verificacion = (distancia_a <= distancia_b)
+
+# Muestra por pantalla
+
+print("\n", verificacion, "\n") # True si es menor o igual que la distancia b y False si es mayor
