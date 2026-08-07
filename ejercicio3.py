8b9ff64071f712bd9bb32ab70ee5af5974c4391a
sk-mati <mati.sidoti.knap@gmail.com>
2026-08-07 16:49:27 -0300

    ejercicio3

---
+++ ejercicio3.py
@@ -0,0 +1,15 @@
+# Define tres variables: nombre, apellido y edad. Usa estas variables para crear una oración
+# completa que diga "Hola, mi nombre es [nombre] [apellido] y tengo [edad] años." Muestra la oración en pantalla.
+
+# Creación de variables
+
+nombre = "Name"
+
+apellido = "Surname"
+
+edad = 100
+
+# Muestra por pantalla la oración
+
+print(f"Hola, mi nombre es {nombre} {apellido} y tengo {edad} años.") 
+# Uso de f-strings para colocar variables en la cadena de texto
