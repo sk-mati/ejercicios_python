88a70c404cc8e6fbceda210ef01e3859ab07ebf2
sk-mati <mati.sidoti.knap@gmail.com>
2026-08-07 20:07:54 -0300

    ejercicio13

---
+++ ejercicio13.py
@@ -0,0 +1,19 @@
+# Dadas 4 variables con diferentes textos (de forma individual), generar una nueva variable con el siguiente contenido: Objetivo: "Python es un lenguaje de programación moderno", cadena_1 = "moderno", cadena_2 = "Python", cadena_3 = "es un lenguaje", cadena_4 = "de programación"
+
+# Definición de variables 
+
+cadena_1 = "moderno"
+
+cadena_2 = "Python"
+
+cadena_3 = "es un lenguaje"
+
+cadena_4 = "de programación"
+
+# Arma la variable nueva con su contenido ordenado
+
+objetivo = (f"{cadena_2} {cadena_3} {cadena_4} {cadena_1}.")
+
+# Muestra por pantalla
+
+print(objetivo)
