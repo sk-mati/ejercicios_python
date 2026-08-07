984b332c502b732d7a934bc5f4f535048bd727e6
sk-mati <mati.sidoti.knap@gmail.com>
2026-08-07 19:30:48 -0300

    ejercicio10

---
+++ ejercicio10.py
@@ -0,0 +1,11 @@
+# Dado un número x, verifica si x está dentro del rango entre 10 y 20 (inclusive). Muestra el resultado de la comparación.
+
+# Definición de la variable
+
+x = 13
+
+# Verificación del rango
+
+print(f"\n¿El número {x} está dentro del rango (10, 20)?\n")
+print("\nTrue = Sí\nFalse = No\n") 
+print(10 < x <= 20, "\n")
