1dc40826b05d623f4c84ee35a2586ce31ebf32fe
sk-mati <mati.sidoti.knap@gmail.com>
2026-08-07 16:21:37 -0300

    readme

---
+++ ejercicio1.py
@@ -0,0 +1,20 @@
+# Crea dos variables a y b con valores enteros. 
+# Intercambia sus valores. Muestra los valores antes y después del intercambio.
+
+a = 5
+
+b = 7
+
+c = a
+
+print("Antes del intercambio: ")
+print("a: ", a)
+print("b: ", b)
+
+a = b
+
+b = c 
+
+print("Después del intercambio: ")
+print("a: ", a)
+print("b: ", b)
